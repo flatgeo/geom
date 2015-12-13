3ca5ae06a0d3d6b008798aea1f2891d87bab04f8
Tim Schaub <tim.schaub@gmail.com>
2015-12-13 14:12:13 -0700

    Decode GeoJSON LineString

--- linestring.go
+++ linestring.go
@@ -2,6 +2,9 @@ package geom
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"strconv"
 )
 
@@ -10,6 +13,11 @@ import (
 type LineString struct {
 	Coordinates []float64
 	Extra       int
+}
+
+type geoJSONLineString struct {
+	Type        string      `json:"type"`
+	Coordinates [][]float64 `json:"coordinates"`
 }
 
 // MarshalJSON returns the GeoJSON representation of the line.
@@ -34,4 +42,39 @@ func (line *LineString) MarshalJSON() ([]byte, error) {
 
 	buffer.WriteString(`]}`)
 	return buffer.Bytes(), nil
+}
+
+// UnmarshalJSON creates a linestring from GeoJSON.
+func (line *LineString) UnmarshalJSON(data []byte) error {
+	geoJSON := geoJSONLineString{}
+	if err := json.Unmarshal(data, &geoJSON); err != nil {
+		return err
+	}
+	if geoJSON.Type != "LineString" {
+		return fmt.Errorf(`Unexpected type: "%s"`, geoJSON.Type)
+	}
+	if len(geoJSON.Coordinates) < 2 {
+		return errors.New("Expected a coordinates array with two or more values")
+	}
+
+	first := geoJSON.Coordinates[0]
+	dimensions := len(first)
+	if dimensions < 2 {
+		return fmt.Errorf("Unexpected length %d for first point", dimensions)
+	}
+
+	coordinates := make([]float64, len(geoJSON.Coordinates)*dimensions)
+	for i := 0; i < len(geoJSON.Coordinates); i++ {
+		coord := geoJSON.Coordinates[i]
+		if len(coord) != dimensions {
+			return fmt.Errorf("Unexpected point length for position %d", i)
+		}
+		for j := 0; j < dimensions; j++ {
+			coordinates[i*dimensions+j] = coord[j]
+		}
+	}
+
+	line.Coordinates = coordinates
+	line.Extra = dimensions - 2
+	return nil
 }
